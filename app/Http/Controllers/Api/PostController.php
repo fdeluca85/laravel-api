f3fe2ed4c9302d3b99991a105238d5b1a14e1817
francesca <fradeluca85@gmail.com>
2022-02-23 18:49:49 +0100

    post published

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
     public function index()
     {
+        $posts = Post::where("published", true)->get();
+
+        return response()->json($posts);
     }
 }
